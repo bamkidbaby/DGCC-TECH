67e6e327611968f43de7872d1a33ec48a7dc2e71
bamkidbaby <126984532+bamkidbaby@users.noreply.github.com>
2026-02-02 09:44:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 
+// Fix __dirname for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Set EJS as the template engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Serve static files from "Public" folder
-app.use(express.static("Public"));
+app.use(express.static(path.join(__dirname, "Public")));
 
+//Bodyparser for form data
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index");
 });
 
 app.get("/printing", (req, res) => {
-  res.render("printing.ejs");
+  res.render("printing");
 });
 
 app.get("/design", (req, res) => {
-  res.render("design.ejs");
+  res.render("design");
 });
 
 app.get("/finishing", (req, res) => {
-  res.render("finishing.ejs");
+  res.render("finishing");
 });
 
 app.get("/training", (req, res) => {
-  res.render("training.ejs");
+  res.render("training");
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
